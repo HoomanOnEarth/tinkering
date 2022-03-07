9c39e545073105839d5e6c6968f57d8650dc68b7
TheAlemazing <hello@thealemazing.com>
2022-03-07 21:25:01 +0700

    run prettier

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,36 +1,36 @@
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
-import { getNotes, Note } from "get-notes";
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+import { getNotes, Note } from 'get-notes'
 
 interface HomeProps {
-	notes: Note[];
+  notes: Note[]
 }
 
 const Home: NextPage<HomeProps> = ({ notes = [] }) => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.notes}>
-				{notes.map((note: Note) => (
-					<div key={note.filename}>
-						<h5 className={styles.note_meta}>{note.updatedAt}</h5>
-						<h1 className={styles.note_title}>{note.title}</h1>
-						<div
-							className={styles.note_content}
-							dangerouslySetInnerHTML={{ __html: note.content }}
-						/>
-					</div>
-				))}
-			</div>
-		</div>
-	);
-};
-
-export async function getStaticProps() {
-	return {
-		props: {
-			notes: getNotes("../../_notes"),
-		},
-	};
+  return (
+    <div className={styles.container}>
+      <div className={styles.notes}>
+        {notes.map((note: Note) => (
+          <div key={note.filename}>
+            <h5 className={styles.note_meta}>{note.updatedAt}</h5>
+            <h1 className={styles.note_title}>{note.title}</h1>
+            <div
+              className={styles.note_content}
+              dangerouslySetInnerHTML={{ __html: note.content }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default Home;
+export async function getStaticProps() {
+  return {
+    props: {
+      notes: getNotes('../../_notes'),
+    },
+  }
+}
+
+export default Home
--- packages/get-notes/index.ts
+++ packages/get-notes/index.ts
@@ -1,46 +1,46 @@
-import fs from "fs";
-import path from "path";
-import MarkdownIt from "markdown-it";
-import matter from "gray-matter";
+import fs from 'fs'
+import path from 'path'
+import MarkdownIt from 'markdown-it'
+import matter from 'gray-matter'
 
-type OrderBy = "NEWEST FIRST" | "OLDEST FIRST";
+type OrderBy = 'NEWEST FIRST' | 'OLDEST FIRST'
 
 function sortByCreateAt(notes: Note[], orderBy: OrderBy): Note[] {
-	return notes.sort((current, next) => {
-		const currentCreatedAt = new Date(current.createdAt).getTime();
-		const nextCreatedAt = new Date(next.createdAt).getTime();
-		return orderBy === "NEWEST FIRST"
-			? nextCreatedAt - currentCreatedAt
-			: currentCreatedAt - nextCreatedAt;
-	});
+  return notes.sort((current, next) => {
+    const currentCreatedAt = new Date(current.createdAt).getTime()
+    const nextCreatedAt = new Date(next.createdAt).getTime()
+    return orderBy === 'NEWEST FIRST'
+      ? nextCreatedAt - currentCreatedAt
+      : currentCreatedAt - nextCreatedAt
+  })
 }
 
-export function getNotes(notesDir: string, orderBy: OrderBy = "NEWEST FIRST") {
-	const postsDirectory = path.join(process.cwd(), notesDir);
-	const filenames = fs.readdirSync(postsDirectory);
-	const markdownFiles = filenames.filter((name) => name.endsWith(".md"));
-	const notes: Note[] = markdownFiles.map((filename) => {
-		const filePath = path.join(postsDirectory, filename);
-		const fileContents = fs.readFileSync(filePath, "utf8");
-		const stats = fs.statSync(filePath);
-		const frontmatter = matter(fileContents);
+export function getNotes(notesDir: string, orderBy: OrderBy = 'NEWEST FIRST') {
+  const postsDirectory = path.join(process.cwd(), notesDir)
+  const filenames = fs.readdirSync(postsDirectory)
+  const markdownFiles = filenames.filter(name => name.endsWith('.md'))
+  const notes: Note[] = markdownFiles.map(filename => {
+    const filePath = path.join(postsDirectory, filename)
+    const fileContents = fs.readFileSync(filePath, 'utf8')
+    const stats = fs.statSync(filePath)
+    const frontmatter = matter(fileContents)
 
-		return {
-			filename,
-			title: frontmatter.data.title,
-			createdAt: stats.ctime.toDateString(),
-			updatedAt: stats.mtime.toDateString(),
-			content: new MarkdownIt().render(frontmatter.content),
-		};
-	});
+    return {
+      filename,
+      title: frontmatter.data.title,
+      createdAt: stats.ctime.toDateString(),
+      updatedAt: stats.mtime.toDateString(),
+      content: new MarkdownIt().render(frontmatter.content),
+    }
+  })
 
-	return sortByCreateAt(notes, orderBy);
+  return sortByCreateAt(notes, orderBy)
 }
 
 export interface Note {
-	filename: string;
-	title: string;
-	content: string;
-	createdAt: string;
-	updatedAt: string;
+  filename: string
+  title: string
+  content: string
+  createdAt: string
+  updatedAt: string
 }
